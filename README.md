46fe683f4330fe739934c307f1fb71fa37d84104
ravanelloo <federica.crisafi98@gmail.com>
2025-09-16 20:01:34 +0200

    initial commit!

---
+++ README.md
@@ -0,0 +1,7 @@
+# TODO
+- Installare Pinia -> https://pinia.vuejs.org/introduction.html
+- Installare Vue Router -> https://router.vuejs.org
+- Installare DaisyUI + Tailwind -> https://daisyui.com
+- Installare pacchetto icone -> https://iconify.design
+- Dettagliare il mockup rendendolo **descrittivo**
+- Creare i file necessari per il setup completo (per lo store, per il router, ecc...)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Pomodoro Timer</h1>
+    <p>25:00</p>
+    <button>Start</button>
+    <button>Pause</button>
+    <button>Reset</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-family: sans-serif;
+}
+
+p {
+  font-size: 2em;
+  text-align: center;
+}
+
+button {
+  margin: 5px;
+  padding: 10px 20px;
+}
+</style>
