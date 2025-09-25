d98aebcbf94409487929a71e78fe87eb6a14bb12
ravanelloo <federica.crisafi98@gmail.com>
2025-09-25 12:48:58 +0200

    timer component done

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,29 +1,71 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
+import { ref, computed, onUnmounted } from 'vue'
+
+const time = ref(2700)
+let intervalId: number | null = null
+
+const seconds = computed(() => String(time.value % 60).padStart(2, '0'))
+const minutes = computed(() => String(Math.floor(time.value / 60)).padStart(2, '0'))
+
+function start() {
+  if (intervalId !== null) return
+  intervalId = window.setInterval(() => {
+    if (time.value > 0) {
+      time.value--
+    } else {
+      pause()
+    }
+  }, 1000)
+}
+
+function pause() {
+  if (intervalId) {
+    clearInterval(intervalId)
+    intervalId = null
+  }
+}
+
+onUnmounted(() => {
+  pause()
+})
+
 </script>
 
 <template>
-<!doctype html>
-<html>
-<div class="card w-96 bg-base-100 card-lg shadow-sm">
-  <div class="card-body">
-    <h2 class="card-title">Timer</h2>
+  <div class="flex justify-center items-center h-screen">
+    <div class="card w-96 bg-base-200 text-base-content shadow-xl">
+      <div class="card-body">
+        <!-- Main Header -->
+        <h2 class="card-title text-4xl justify-center">Timer</h2>
 
-    <div class="flex w-full flex-col">
+        <div class="divider"></div>
 
-  <div class="divider"></div>
-
+        <!-- Countdown -->
+        <div class="flex justify-center items-center text-4xl">
+  <span>{{ minutes }}</span> : <span>{{ seconds }}</span>
 </div>
-    <span class="countdown font-mono text-2xl">
-  <span style="--value:45;" aria-live="polite" aria-label="45">45</span>
-  :
-  <span style="--value:00;" aria-live="polite" aria-label="00">00</span>
-</span>
 
-    <p>Create your session</p>
-    <button class="btn">Active study</button>
-    <button class="btn">Short break</button>
-    <button class="btn">Long break</button>
+
+
+        <!-- Start Button -->
+        <div class="card-actions justify-center mt-4 flex gap-2">
+          <button class="btn btn-primary" @click="start">Start</button>
+          <button class="btn btn-secondary" @click="pause">Pause</button>
+        </div>
+
+        <!-- Second Header -->
+        <h3 class="card-title text-2xl font-bold mt-4 justify-center">
+          Create your session!
+        </h3>
+
+        <!-- Buttons -->
+        <div class="flex flex-wrap gap-1 justify-center mt-1">
+          <button class="btn">Active study</button>
+          <button class="btn">Short break</button>
+          <button class="btn">Long break</button>
+        </div>
+      </div>
+    </div>
   </div>
-</html>
 </template>
